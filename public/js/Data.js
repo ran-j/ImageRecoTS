944eb2f7e45259dfd504fc5b2e01587282a7affd
Ranieri Abreu Silva Junior <rabreu@indracompany.com>
2018-11-09 09:36:36 -0200

    Add more itens to model

--- public/js/Data.js
+++ public/js/Data.js
@@ -2,5 +2,7 @@ var imgList = [
     { Path: "img/gato1.jpg", ClassName: "Gato Amarelo" },
     { Path: "img/gato2.jpg", ClassName: "Gato Amarelo" },
     { Path: "img/cachorro1.jpg", ClassName: "Cachorro Amarelo" },
-    { Path: "img/cachorro2.jpg", ClassName: "Cachorro Amarelo" }
+    { Path: "img/cachorro2.jpg", ClassName: "Cachorro Amarelo" },
+    { Path: "img/car1.jpg", ClassName: "Mustang GT" },
+    { Path: "img/car2.jpeg", ClassName: "Mustang GT" }
 ];
--- public/js/imagenet_classes.js
+++ public/js/imagenet_classes.js
@@ -1,4 +1,5 @@
 var IMAGENET_CLASSES = {
   0: "Gato Amarelo",
-  1: "Cachorro Amarelo"
+  1: "Cachorro Amarelo",
+  2: "Mustang GT"
 };
